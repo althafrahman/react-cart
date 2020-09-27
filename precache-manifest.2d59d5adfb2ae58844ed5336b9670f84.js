69f835397fd6c5d52a8bff17d002f58912ce02dc
Althaf <althafmarahman@gmail.com>
2020-09-27 10:42:44 +0530

    Updates

---
+++ precache-manifest.2d59d5adfb2ae58844ed5336b9670f84.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c90725038f0adfd16f5ef86acc48f75",
+    "url": "/react-cart/index.html"
+  },
+  {
+    "revision": "c4e7ac2cdb7ff8fe002d",
+    "url": "/react-cart/static/css/2.6f900c52.chunk.css"
+  },
+  {
+    "revision": "5924afdfd9ee6f39782b",
+    "url": "/react-cart/static/css/main.047be0b0.chunk.css"
+  },
+  {
+    "revision": "c4e7ac2cdb7ff8fe002d",
+    "url": "/react-cart/static/js/2.152c6351.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-cart/static/js/2.152c6351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5924afdfd9ee6f39782b",
+    "url": "/react-cart/static/js/main.e7d4dc01.chunk.js"
+  },
+  {
+    "revision": "d97a1f68e8ba613661fc",
+    "url": "/react-cart/static/js/runtime-main.fa80823d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cart/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-cart/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-cart/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-cart/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-cart/static/media/slick.f97e3bbf.svg"
+  }
+]);
